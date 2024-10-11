7fa335fad5428323cbbd9c206387e1c021dcbb48
oceaneyessss <mwu119@uottawa.ca>
2024-10-11 05:38:38 -0400

    Some updates, see below
    1. UI optimization, like button position, constraints, font size, color, background etc.
    
    2. Added the Back button on registration page. Now it's able to return to sign page during registration.
    
    3. Added password visibility toggle, (The truck would be red when it's off and green when it's on.
    
    4. Deleted "main activity". The launch page will now be the login page
    
    5. Fixed a error that the registration and login buttons were mixed up
    
    6. Changed some variable's default name, it will be easier to identify and use
    
    7. Added Icons for email and password entry on login page

--- app/src/main/java/com/example/myapplication/LoginPage.java
+++ app/src/main/java/com/example/myapplication/LoginPage.java
@@ -1,6 +1,14 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +18,49 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginPage extends AppCompatActivity {
 
+    private EditText editTextPassword;
+    private Switch EyeSw;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_page);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Button to switch to registration
+        Button button1 = findViewById(R.id.registerButton);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginPage.this, Registrationpage.class));
+            }
+        });
+
+        //Password visibility Switch
+        editTextPassword = findViewById(R.id.editTextPassword);
+        EyeSw = findViewById(R.id.EyeSw);
+
+        EyeSw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (editTextPassword.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance())) {
+
+                    editTextPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    EyeSw.getTrackDrawable().setTint(getResources().getColor(android.R.color.holo_red_light));
+                } else {
+                    editTextPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    EyeSw.getTrackDrawable().setTint(getResources().getColor(android.R.color.holo_green_light));
+                }
+
+                editTextPassword.setSelection(editTextPassword.getText().length());
+            }
         });
     }
 }
--- app/src/main/java/com/example/myapplication/Registrationpage.java
+++ app/src/main/java/com/example/myapplication/Registrationpage.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,15 @@ public class Registrationpage extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button1 = findViewById(R.id.backBtn);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Registrationpage.this, LoginPage.class));
+            }
         });
     }
 }
