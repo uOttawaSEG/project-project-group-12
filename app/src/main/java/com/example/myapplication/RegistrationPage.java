2cf93047ce02fec1eacb0acf545ca7e2a2feee5b
Zixian Chen <bluch336@gmail.com>
2024-10-14 20:12:22 -0400

    Created variables for UI elements in registration page

--- app/src/main/java/com/example/myapplication/RegistrationPage.java
+++ app/src/main/java/com/example/myapplication/RegistrationPage.java
@@ -23,19 +23,42 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class RegistrationPage extends AppCompatActivity {
 
     private EditText firstNameField, lastNameField, emailField, passwordField, confirmPasswordField, addressField, phoneNumberField;
+    private RadioGroup rolegroup;
+
+    private Button confirmSignUpButton, backButton;
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registrationpage);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button backButton = findViewById(R.id.backBtn);
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        firstNameField = findViewById(R.id.firstName);
+        lastNameField = findViewById(R.id.lastName);
+        emailField = findViewById(R.id.emailAddress);
+        passwordField = findViewById(R.id.password);
+        confirmPasswordField = findViewById(R.id.ConfirmPassword);
+        addressField = findViewById(R.id.Address);
+        phoneNumberField = findViewById(R.id.PhoneNumber);
+        rolegroup = findViewById(R.id.radioGroup);
+        confirmSignUpButton = findViewById(R.id.ConfirmSignUp);
+        backButton = findViewById(R.id.backBtn);
+
+
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,4 +75,7 @@ public class RegistrationPage extends AppCompatActivity {
 
          */
     }
+
+
+
 }
