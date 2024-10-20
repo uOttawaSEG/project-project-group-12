70130990bb848c330815ba0cc25c352051493182
Alexander Radar <alexander.radar11@gmail.com>
2024-10-20 12:43:56 -0400

    AdminPage

---
+++ app/src/main/java/com/example/myapplication/AdminPage.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AdminPage extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_admin_page);
+
+        //Retrieves name and role of the user from last page
+        String userRole = getIntent().getStringExtra("userRole");
+
+        //Displays it effectively
+        TextView welcome = findViewById(R.id.topText);
+        welcome.setText("Welcome to admin page");
+        welcome.setGravity(Gravity.CENTER); // Centre the text
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button logOutButton = findViewById(R.id.logOutBtn);
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(AdminPage.this, LoginPage.class));
+        }
+    });
+
+    }
+}
