a57d5aa49251cb8cc54424b6f94f3ced810a0a6a
icprotocol <icama026@uottawa.ca>
2024-11-04 12:52:13 -0500

    New classes for Delivrable 3 -  based on updated UML

---
+++ EAMS/app/src/main/java/com/example/myapplication/Event.java
@@ -0,0 +1,66 @@
+package com.example.myapplication;
+
+import java.util.Date;
+
+public class Event {
+    private String title, description, eventAddress;
+    private Date startTime, endTime;
+
+
+
+    public Event(String title, String description , String eventAddress, Date startTime , Date endTime){
+        this.title = title;
+        this.description = description;
+        this.eventAddress = eventAddress;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+
+
+
+
+    public Event(){
+        //empty constructor for FireBase
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEventAddress() {
+        return eventAddress;
+    }
+
+    public void setEventAddress(String eventAddress) {
+        this.eventAddress = eventAddress;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+}
---
+++ EAMS/app/src/main/java/com/example/myapplication/EventRegistration.java
@@ -0,0 +1,61 @@
+package com.example.myapplication;
+
+public class EventRegistration {
+    private String status;
+
+    private Event event;
+    private String eventId; // may not need it hence why not in constructor
+
+    private Attendee attendee;
+    private String attendeeId; // may not need it hence why not in constructor
+
+
+    public EventRegistration(String status , Event event , Attendee attendee){
+        this.status = status;
+        this.event = event;
+        this.attendee = attendee;
+    }
+
+
+
+    public  EventRegistration(){
+        //empty constructor for FireBase
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public Attendee getAttendee() {
+        return attendee;
+    }
+
+    public void setAttendee(Attendee attendee) {
+        this.attendee = attendee;
+    }
+
+    public String getAttendeeId() {
+        return attendeeId;
+    }
+}
